cb3c8914b6c816148d9cb45e73f789ce3fa06cee
Greg Lyons <gregory.lyons512@gmail.com>
2022-05-04 11:31:32 -0500

    Finished HTML and added design system to style-guide.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        activehtext: "hsl(238, 29%, 16%)",
+        inactivehtext: "hsl(237, 12%, 33%)",
+        ptext: "hsl(240, 6%, 50%)",
+        divider: "hsl(240, 5%, 91%)",
+        redtext: "hsl(14, 88%, 65%)",
+        gradientpurple: "hsl(237, 12%, 33%)",
+        gradientblue: "hsl(240, 73%, 65%)",
+
+      },
+      fontFamily: {
+        kumbh: ['Kumbh Sans', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
